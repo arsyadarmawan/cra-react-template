b0c9f504f746cf46044d0c4eef37ff2ea65bb809
arsyadthareeq <thareq.darmawan@amarbank.co.id>
2025-03-01 14:07:15 +0700

    add validation for form

--- src/app/listener.js
+++ src/app/listener.js
@@ -1,19 +1,12 @@
 import store from './store';
 
-let currentAuth;
+// let currentAuth;
 function listen(){
     store.subscribe(()=>{
         console.log('State berubah:', store.getState());
     });
 }
 
-function listener() {
-    let previousAuth = currentAuth;
-    currentAuth = store.getState().auth;
-    if(currentAuth !== previousAuth){
-        localStorage.setItem('auth', JSON.stringify(currentAuth));
-    }
-}
 export {
     listen
 }
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,48 +1,51 @@
 import * as React from 'react';
 import { LayoutOne, Card, FormControl, InputText, InputPassword, Button } from 'upkit';
+import { useForm } from 'react-hook-form';
+import rules from './validation';
 
 export default function Register(){
+    let { register, handleSubmit, errors, setError } = useForm();
+    const onSubmit = async formData => {
+        console.log(JSON.stringify(formData))
+        alert(JSON.stringify(formData));
+    }
     return (
         <LayoutOne size="small">
             <Card color="white">
-                <form>
-                    <FormControl>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <FormControl errorMessage={errors.full_name?.message}>
                         <InputText
                             name="full_name"
                             placeholder="Nama Lengkap"
                             fitContainer
+                            ref={register(rules.full_name)}
                         />
                     </FormControl>
 
-                    <FormControl>
+                    <FormControl errorMessage={errors.email?.message}>
                         <InputText
                             name="email"
                             placeholder="Email"
+                            ref={register(rules.email)}
                             fitContainer
                         />
                     </FormControl>
 
-                    <FormControl>
+                    <FormControl errorMessage={errors.password?.message}>
                         <InputPassword
                             name="password"
                             placeholder="Password"
+                            ref={register(rules.password)}
                             fitContainer
                         />
                     </FormControl>
 
 
-                    <FormControl>
+                    <FormControl errorMessage={errors.password_confirmation?.message}>
                         <InputPassword
                             name="password_confirmation"
                             placeholder="Konfirmasi Password"
-                            fitContainer
-                        />
-                    </FormControl>
-
-                    <FormControl>
-                        <InputPassword
-                            name="password_confirmation"
-                            placeholder="Konfirmasi Password"
+                            ref={register(rules.password_confirmation)}
                             fitContainer
                         />
                     </FormControl>
